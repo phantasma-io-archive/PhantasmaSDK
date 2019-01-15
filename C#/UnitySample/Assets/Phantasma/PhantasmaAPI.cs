459f6f8126a107d8991c89c0d3a43639371f837c
Joao Catarino <joaopereiracatarino@gmail.com>
2019-01-15 18:46:53 +0000

    Updated Phantasma.Blockchain.dll and fixed transactions creation on PhantasmaAPI.cs

--- C#/UnitySample/Assets/Phantasma/PhantasmaAPI.cs
+++ C#/UnitySample/Assets/Phantasma/PhantasmaAPI.cs
@@ -679,8 +679,8 @@ namespace Phantasma.SDK
        {
            Debug.Log("Sending transaction...");
 
-           var tx = new Blockchain.Transaction("nexus", chain, script, DateTime.UtcNow.AddHours(1), 0);
-           tx.Sign(PhantasmaDemo.Instance.Key);
+           var tx = new Blockchain.Transaction("nexus", chain, script,  DateTime.UtcNow + TimeSpan.FromHours(1));
+            tx.Sign(PhantasmaDemo.Instance.Key);
 
            yield return SendRawTransaction(Base16.Encode(tx.ToByteArray(true)), callback, errorHandlingCallback);
        }
